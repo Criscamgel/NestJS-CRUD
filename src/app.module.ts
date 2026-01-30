32978e19d176fa8d897c819372d9a4bb4e999676
Camilo Guio <camiloguio@gmail.com>
2026-01-30 08:11:27 -0500

    Finish the CRUD

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config'; 
+import { CommonModule } from './common/common.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+    rootPath: join(__dirname,'..','public'),
+    }),
+    PokemonModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-api-server'),
+    CommonModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
