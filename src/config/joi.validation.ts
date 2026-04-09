3a3db92bacd27b5ed05370686d97c4b4ba97fb94
Camilo Guio <camilo.guio@samtel.eu>
2026-04-08 20:02:30 -0500

    fix: optimize Dockerfile to avoid dual parallel yarn installs on VPS, add JWT_SECRET to Joi validation

--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -2,6 +2,7 @@ import * as Joi from 'joi';
 
 export const JoiValidationSchema = Joi.object({
     MONGODB: Joi.required(),
+    JWT_SECRET: Joi.string().required(),
     PORT: Joi.number().default(3005),
     DEFAULT_LIMIT: Joi.number().default(6),
     MAILER_SERVICE: Joi.string().optional(),
